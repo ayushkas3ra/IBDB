8d48b5b3ffad981a3a0e20e87934ef1b85e251a8
ayushkas3ra <raun4k@protonmail.com>
2026-08-09 18:51:00 +0530

    Configure production allowed hosts

--- backend/ibdb/ibdb/settings.py
+++ backend/ibdb/ibdb/settings.py
@@ -28,7 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "ibdb-xo5z.onrender.com",
+]
 
 
 # Application definition
