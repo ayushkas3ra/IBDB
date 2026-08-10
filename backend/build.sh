8b5533232f322cfef866c62d220e02bf070fd1e9
ayushkas3ra <raun4k@protonmail.com>
2026-08-10 13:34:50 +0530

    import books during deployment

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install -r requirements.txt
+
 cd ibdb
 
 python manage.py migrate
-
 python manage.py import_books
-
 python manage.py collectstatic --no-input
